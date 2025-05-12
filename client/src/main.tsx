b58b5a0445eac26f815340fa2802ba4f44f479b7
hennioo <henningdeliusfritz@gmail.com>
2025-05-12 10:27:54 +0200

    Fix: correct path to index.css in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./src/index.css";
+import "./index.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
